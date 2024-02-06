45979fb477141c90871d6f533a5b04f4932762e7
Liisi Loomets <146342702+LLoomets@users.noreply.github.com>
2024-02-06 21:41:38 +0200

    Kodus katse

---
+++ 01_funktsioon/Kodutoo_01/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+
+//Siia tuleb esimene kodutöö, praegu katsetan kodus kas asjad toimivad.
