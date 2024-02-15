dbd3a552e9914732b4a456d23b930bd16ad0452f
Liisi Loomets <146342702+LLoomets@users.noreply.github.com>
2024-02-15 15:09:44 +0200

    kodutöö pool tehtud

--- 02_klass/Kodutoo_02/src/Auto.java
+++ 02_klass/Kodutoo_02/src/Auto.java
@@ -2,22 +2,27 @@ public class Auto {
 
     String tootja;
     String mudel;
-    int aasta;
+    int tootmisAasta;
 
-    public Auto(String tootja, String mudel, int aasta) {
+    public Auto(String tootja, String mudel, int tootmisAasta) {
         this.tootja = tootja;
         this.mudel = mudel;
-        this.aasta = aasta;
+        this.tootmisAasta = tootmisAasta;
     }
 
-    public String autoVanuseMaaramine(){
-        if (aasta >= 2010) {
+    public String autoOlek(){
+        if (tootmisAasta >= 2010) {
             return "Täitsa uus auto";
-        } else if (aasta >= 2000) {
+        } else if (tootmisAasta >= 2000) {
             return "No veel sõidab";
         } else {
             return "Ikka päris vana auto juba";
         }
+    }
+
+    public int kauaAutoTeelOlnud(){
+        int praeguAasta = 2024;
+        return praeguAasta - tootmisAasta;
     }
 
 
--- 02_klass/Kodutoo_02/src/Main.java
+++ 02_klass/Kodutoo_02/src/Main.java
@@ -1,9 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-    Auto esimeneAuto = new Auto("Toyota","Avensis",2006);
-    Auto teineAuto = new Auto("Toyota","Yaris",2017);
 
+        Auto esimeneAuto = new Auto("Toyota","Avensis",2006);
+        Auto teineAuto = new Auto("Toyota","Yaris",2017);
+
+        String esimeseAutoOlek = esimeneAuto.autoOlek();
+        System.out.println(esimeseAutoOlek);
+        String teiseAutoOlek = teineAuto.autoOlek();
+        System.out.println(teiseAutoOlek);
+
+        int esimeseAutoVanus = esimeneAuto.kauaAutoTeelOlnud();
+        System.out.println(esimeseAutoVanus + " aastat teel olnud");
+        int teiseAutoVanus = teineAuto.kauaAutoTeelOlnud();
+        System.out.println(teiseAutoVanus + " aastat teel olnud");
+    }
 
 }
