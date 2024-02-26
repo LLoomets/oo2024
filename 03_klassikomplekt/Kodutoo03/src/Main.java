38dfb325345ac248a0bbcc111b196005a0de4eac
Liisi Loomets <146342702+LLoomets@users.noreply.github.com>
2024-02-26 19:58:29 +0200

    Kodutöö03, lisasin veidi juurde

--- 03_klassikomplekt/Kodutoo03/src/Main.java
+++ 03_klassikomplekt/Kodutoo03/src/Main.java
@@ -2,12 +2,20 @@ public class Main {
     public static void main(String[] args) {
 
         Toode toode1 = new Toode("Diivan", 599.99);
-        Tellimus tellimus = new Tellimus(toode1, 49.99);
+        Toode toode2 = new Toode("Voodi", 499.99);
 
-        double toode1KoguSumma = tellimus.arvutaKoguHind();
+        Tellimus tellimus1 = new Tellimus(toode1, 49.99);
+        Tellimus tellimus2 = new Tellimus(toode2, 39.99);
+
+        double toode1KoguSumma = tellimus1.arvutaKoguHind();
         System.out.println(toode1KoguSumma + " €");
+        double toode2KoguSumma = tellimus2.arvutaKoguHind();
+        System.out.println(toode2KoguSumma + " €");
 
         toode1.muudaTooteNimi("Televiisor");
         System.out.println(toode1.tooteNimi);
+
+        toode2.muudaTooteNimi("Söögilaud");
+        System.out.println(toode2.tooteNimi);
     }
 }
