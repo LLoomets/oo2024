dbc65dab67998742bf40e45475b88c5182f4d69e
Liisi Loomets <146342702+LLoomets@users.noreply.github.com>
2024-04-23 12:13:59 +0300

    Kodutöö lõpetatud

--- 07_andmebaas/Kodutoo05/Kodutoo05/src/main/java/ee/tlu/Kodutoo05/controller/RestTemplateController.java
+++ 07_andmebaas/Kodutoo05/Kodutoo05/src/main/java/ee/tlu/Kodutoo05/controller/RestTemplateController.java
@@ -5,9 +5,11 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -21,5 +23,21 @@ public class RestTemplateController {
         ResponseEntity<Todo[]> response = restTemplate.exchange(url, HttpMethod.GET, null, Todo[].class);
 
         return  List.of(response.getBody());
+    }
+
+    @GetMapping("saa-tegevused/{kasutajaId}")
+    public List<Todo> getUserTodo(@PathVariable int kasutajaId) {
+
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "https://jsonplaceholder.typicode.com/todos";
+        ResponseEntity<Todo[]> response = restTemplate.exchange(url, HttpMethod.GET, null, Todo[].class);
+
+        List<Todo> kasutajaTodod = new ArrayList<>();
+        for (Todo t : response.getBody()) {
+            if (t.getUserId() == kasutajaId) {
+                kasutajaTodod.add(t);
+            }
+        }
+        return kasutajaTodod;
     }
 }
